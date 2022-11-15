ad235bb33bf0078a9e9112c79b39674eb35047da
iesteed <94650446+iesteed@users.noreply.github.com>
2022-11-14 22:09:29 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,7 @@ const users = [];
 const siteConfig = {
     title: 'Ian E. Steed', // Title for your website.
     tagline: 'New, Passionate, aspiring college student.',
-    profilePhoto: './assets/mewo.jpg',
+    profilePhoto: 'assets/mewo.jpg',
     url: 'https://iesteed.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
